0e47f6fcd1e612d68a9cc499c8f2b7b7e24471aa
Cyborgnetical <140975796+Cyborgnetical@users.noreply.github.com>
2025-12-28 11:41:25 -0800

    alr working repo

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("working 123, we are alive");
+import express from "express"
+const app = express()
+
+app.get("/",(req, res)=>{
+    res.send("app alive")
+})
+
+app.listen(8080,()=>{
+    console.log("App hosted at localhost:8080")
+})
